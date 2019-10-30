a8f7e81b8f88d7877ce3dab1b7713394c9c458ea
Dustin Lunoe <dlunoe@yahoo.com>
2019-10-29 18:18:45 -0600

    styled Navbar

--- react-forums-client/src/Components/Navigation/Navigation.js
+++ react-forums-client/src/Components/Navigation/Navigation.js
@@ -6,12 +6,13 @@ import {Button} from 'reactstrap';
 const Navigation = () => (
     <div class="Navigation">
         <ul class="nav-list">
-            <li>
+            <li class="topic-button">
                 <NavLink to={'/topics'} name="nav-forums">
                     <Button>Topics</Button>
                 </NavLink>                
             </li>
-            <li class="nav-button">
+            <h1 class="site-title">Welcome to My Forums</h1>
+            <li class="new-button">
                 <NavLink to={'/topics/new'}>
                     <Button>New Topic</Button>
                 </NavLink>
